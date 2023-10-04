21c054dc25741a009d711c6e4b0076c9e01a820c
Fami6Xx <libojer@gmail.com>
2023-10-04 10:17:38 +0200

    feat: init of conv

--- PasswordGeneration/Program.cs
+++ PasswordGeneration/Program.cs
@@ -1,8 +1,74 @@
-﻿Console.WriteLine("Hello world! Lets type something");
-string? typedString = Console.ReadLine();
-if (typedString == null)
+﻿using PasswordGeneration;
+
+FileAPI api = new FileAPI();
+
+Begin();
+
+void Begin()
 {
-    Console.WriteLine("Shiiit you niggga");
-    return;
+    Console.WriteLine("Hello! Thanks for using our program for creating your new passwords.");
+    Console.WriteLine("What do you want to do? (Write the number before the action)");
+    Console.WriteLine("1 - Show all saved passwords");
+    Console.WriteLine("2 - Create a new password");
+    string output = Console.ReadLine();
+    while (!CheckForCorrectAnswer(new[] { "1", "2" }, output))
+    {
+        Console.WriteLine("Seems like this answer is not what we expected, try again");
+        output = Console.ReadLine();
+    }
+    
+    if (output.Equals("1"))
+    {
+        Console.Clear();
+        Console.WriteLine("Showing all saved passwords");
+        ShowAllPasswords();
+        return;
+    }
+    // Generate pass func
 }
-Console.WriteLine("Hell yeah ma boii " + typedString);
+
+bool CheckForCorrectAnswer(string[] answers, string currentAnswer)
+{
+    return answers.Any(s => s.Equals(currentAnswer));
+}
+
+void ShowAllPasswords()
+{
+    string[] hashedPasswords = api.ReadAllPasswordsFromFile();
+    if (hashedPasswords.Length == 0)
+    {
+        Console.WriteLine("You do not have any saved passwords.");
+        AskToContinue();
+        return;
+    }
+    
+    foreach (string hashedPassword in hashedPasswords)
+    {
+        // ToDo: Decrypt
+        Console.WriteLine(hashedPassword);
+    }
+    Console.WriteLine("--");
+    AskToContinue();
+}
+
+void AskToContinue()
+{
+    Console.WriteLine("Would you like to:");
+    Console.WriteLine("1 - Start from beginning");
+    Console.WriteLine("2 - Exit the program");
+    string output = Console.ReadLine();
+    while (!CheckForCorrectAnswer(new[] { "1", "2" }, output))
+    {
+        Console.WriteLine("Seems like this answer is not what we expected, try again");
+        output = Console.ReadLine();
+    }
+
+    if (output.Equals("1"))
+    {
+        Console.Clear();
+        Begin();
+        return;
+    }
+
+    Environment.Exit(0);
+}
