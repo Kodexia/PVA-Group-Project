53cb482dc14481186af3d9e72348cf56acad1c57
Fami6Xx <libojer@gmail.com>
2023-10-04 09:22:47 +0200

    feat: init of fileapi

---
+++ PasswordGeneration/FileAPI.cs
@@ -0,0 +1,13 @@
+﻿namespace PasswordGeneration;
+
+public class FileAPI
+{
+    public void Startup()
+    {
+        return;
+    }
+    public void SaveNewPasswordToFile(string password)
+    {
+        return;
+    }
+}
